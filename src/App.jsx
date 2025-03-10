3eca0e8f62750c5667f921fcda764f4f7ec7df8e
Aafreen2203 <aafreen22k@gmail.com>
2025-03-10 23:00:48 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
+// IntegratedApp.jsx
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 import { auth } from "../firebaseconfig"; // Ensure correct import path
 import Header from "./Header";
 import HeroSection from "./HeroSection";
 import FeaturesSection from "./FeaturesSection";
-import LoadingPage from "./LandingPage2/LoadingPage"; // About Page
+import LoadingPage from "./LandingPage2/LoadingPage"; 
 import LoginContainer from "./LoginPage/LoginContainer"; // Login Page
 import Container from "./SigninPage/Container"; // Sign-in Page
 import AdminDashboard from "./Dashboard/AdminDashboard";
 import InputDesign from "./CreateQuiz/InputDesign";
 import Dashboard from "./ManageQuiz/Dashboard";
 import UserManagementDashboard from "./ManageUser/UserManagementDashboard";
+
 
 
 const Layout = () => {
@@ -26,9 +28,9 @@ const Layout = () => {
 
   return (
     <>
-      {/* {user && <Header />} Show header only when user is logged in */}
+      {/* {user && <Header />}  */}
       <Routes>
-        <Route path="/" element={<> <Header /><HeroSection /><FeaturesSection /></>} />
+        <Route path="/" element={<><Header /><HeroSection /><FeaturesSection /></>} />
         <Route path="/about" element={<LoadingPage />} />
         <Route path="/login" element={<LoginContainer />} />
         <Route path="/signin" element={<Container />} />
@@ -36,14 +38,7 @@ const Layout = () => {
         <Route path="/createquiz" element={<InputDesign />} />
         <Route path="/managequiz" element={<Dashboard />} />
         <Route path="/manageuser" element={<UserManagementDashboard />} />
-        {/* <Route 
-          path="/profile" 
-          element={<ProtectedRoute user={user}><AdminDashboard /></ProtectedRoute>} 
-        />
-        <Route 
-          path="/dashboard" 
-          element={<ProtectedRoute user={user}><ProfileContent /></ProtectedRoute>} 
-        /> */}
+        
       </Routes>
     </>
   );
